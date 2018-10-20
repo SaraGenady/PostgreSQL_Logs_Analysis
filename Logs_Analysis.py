47ae2dc179fbc7b9a7265261c3a68d4c1fda7434
Sara-Mohamed-Genady <saragnady92@gmail.com>
2018-10-20 23:08:10 +0200

    Requires Changes

--- Logs_Analysis.py
+++ Logs_Analysis.py
@@ -1,18 +1,30 @@
+#!/usr/bin/env python3
+
 # import psycopg2 #
 import psycopg2
+
 # connect db news #
-conn= psycopg2.connect(database="news")
+conn = psycopg2.connect(database="news")
+
 # init cursor #
 c = conn.cursor()
 
-# What are the most popular three articles of all time? 
-c.execute("""select articles.title, count(*) as views_num from log,articles where log.status='200 OK' and articles.slug = substr(log.path, 10) group by log.path,articles.title order by views_num desc limit 3""")
-top_three_articles=c.fetchall()
+# What are the most popular three articles of all time?#
+c.execute("""select articles.title,
+        count(*) as views_num
+        from log,articles
+        where log.status='200 OK'
+        and articles.slug = substr(log.path, 10)
+        group by log.path,articles.title
+        order by views_num desc
+        limit 3""")
+top_three_articles = c.fetchall()
 # Print question
 print("What are the most popular three articles of all time?")
+
 # Loop on articles and print
 for title, views_num in top_three_articles:
-     print(" \"{}\" -- {} views".format(title, views_num))
+    print(" \"{}\" -- {} views".format(title, views_num))
 
 # Who are the most popular article authors of all time?
 c.execute("""select authors.name, count(*) as views_num
@@ -22,30 +34,31 @@ c.execute("""select authors.name, count(*) as views_num
               and articles.slug = substr(log.path, 10)
               group by authors.name
               order by views_num desc""")
-top_authors =c.fetchall()
+
+top_authors = c.fetchall()
+
 # Print question
 print("Who are the most popular article authors of all time?")
+
 # Loop on authors and print
 for title, views_num in top_authors:
-     print(" \"{}\" -- {} views".format(title, views_num))
+    print(" \"{}\" -- {} views".format(title, views_num))
 
 # On which days did more than 1% of requests lead to errors?
-# used limit because it takes more time 
-c.execute("""select dateerror,percentage from tb_percentage limit 1 """)
-error_requests =c.fetchall()
+# used limit because it takes more time
+c.execute("""select time, percentagefailed
+             from percentagecounts
+             where percentagefailed > 1""")
+
+error_requests = c.fetchall()
+
 # Print question
 print("On which days did more than 1% of requests lead to errors?")
 # Loop on error_requests and print
-for dateerror, percentage in error_requests:
-     if percentage > 1 :
-          print(" \"{}\" -- {} % errors".format(dateerror, percentage))
-     else :
-          print("Not found")
-     
-
-
+for time, percentagefailed in error_requests:
+    print(" \"{}\" -- {} % errors".format(time, percentagefailed))
 # conn commit #
 conn.commit()
 
-# conn close #
+# conn close # W292	63	17	no newline at end of file How to solve it ??
 conn.close()
--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 Tools :
 --------
-1- Visual Studio Code
-2- PostgreSQL
+1- Visual Studio Code 
+2- PostgreSQL 
 3- Linux terminal 
-4- virtual machine (VM) & Vagrant
+4- VirtualBox VM 
+5- Vagrant 
+6- Database (https://d17h27t6h515a5.cloudfront.net/topher/2016/August/
+57b5f748_newsdata/newsdata.zip)
 
 Project Requirements :
 ----------------------
@@ -13,12 +16,14 @@ Project Requirements :
 
 How to run the program :
 -------------------------
-1- Install Visual Studio Code : Install on it Python
-2- Install virtual machine (VM) & Vagrant after that configration then 
-   vagrant up and vagrand ssh 
+1- Install Visual Studio Code (https://code.visualstudio.com/download)
+2- Install virtual machine (VM) (https://www.virtualbox.org/wiki/Download_Old_Builds_5_1)
+3- Vagrant (https://www.vagrantup.com/) 
+4- configration unzip (https://s3.amazonaws.com/video.udacity-data.com/topher/2018/April/5acfbfa3_fsnd-virtual-machine/fsnd-virtual-machine.zip)
+4- cd Inside vagrant then vagrant up and vagrand ssh 
 3- run command : cd /vagrant
-4- for sql download newsdata.sql then run command psql -d news -f 
-   newsdata.sql
+4- for sql unzip database and put newsdata.sql in vagrant directory 
+   then run command : psql -d news -f newsdata.sql
 5- Put Logs_Analysis.py in vagrant directory then run command 
    python3 Logs_Analysis.py
 
@@ -26,14 +31,15 @@ Views:
 -------------------------
 run this command to create view: 
 
-psql -d news -c "create view errors_request_day as select time ::date as "DateError" ,count(1) as "num_errors"  from log where log.status='404 NOT FOUND' group by log.time order by "num_errors" desc"
+------------View 1------------------------------------
+psql -d news -c "CREATE VIEW statustotal AS SELECT time ::date,status FROM log"
 
--------used limit & offset for time------------
-psql -d news -c "create view tb_percentage as select dateerror,(num_errors * 100 /(select  count(1) from log where time ::date = dateerror)) as "percentage" from errors_request_day limit 5 offset 0 "
+------------View 2------------------------------------
+psql -d news -c "CREATE VIEW statusfailed AS SELECT time, count(*) AS num FROM statustotal WHERE status = '404 NOT FOUND' GROUP BY time;"
 
+------------View 3------------------------------------
+ psql -d news -c "CREATE VIEW statusall as SELECT time, count(*) AS num FROM statustotal WHERE status = '404 NOT FOUND' OR status = '200 OK' GROUP BY time;"
 
-
-
-
-
+-------------View4-------------------------------------
+psql -d news -c "CREATE VIEW percentagecounts AS SELECT statusall.time, statusall.num AS numall, statusfailed.num AS numfailed, statusfailed.num::double precision/statusall.num::double precision * 100 AS percentagefailed FROM statusall, statusfailed WHERE statusall.time = statusfailed.time;"
 
